2575a393e4ddc5d87c58d589e8a1a892cb86c807
subarna <khadka.subarnabijaya@gmail.com>
2019-06-18 23:53:06 -0400

    my project

---
+++ LizardDemo/Lizard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LizardDemo
+{
+    class Lizard
+    {
+        public string Species;
+        public int Quantity;
+        public bool IsDangerous;
+        public override string ToString()
+        {
+            return string.Format($"{Species} {Quantity} {(IsDangerous ? "dangerous" : "not dangerous")}");
+        }
+        public static List<Lizard> GetLizards()
+        {
+            return new List<Lizard>()
+            {
+                new Lizard() { Species = "Leopard gecko", Quantity = 3, IsDangerous = true },
+                new Lizard() { Species = "Bearded dragon", Quantity = 1, IsDangerous = true },
+                new Lizard() { Species = "Crested gecko", Quantity = 4, IsDangerous = false },
+                new Lizard() { Species = "Argentine tegu", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Thorny devil", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Casquehead lizard", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Panther chameleon", Quantity = 6, IsDangerous = false },
+                new Lizard() { Species = "Veiled chameleon", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Water monitor", Quantity = 4, IsDangerous = false },
+                new Lizard() { Species = "Komodo dragon", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Green iguana", Quantity = 4, IsDangerous = false },
+                new Lizard() { Species = "Blotched blue-tongue lizard", Quantity = 4, IsDangerous = true },
+                new Lizard() { Species = "Gila monster", Quantity = 4, IsDangerous = true }
+            };
+        }
+    }
+
+}
---
+++ LizardDemo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace LizardDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Lizard> lizards = Lizard.GetLizards();
+            //serializeOneLizard("one.txt", lizards[0]);
+            //deserializeOneLizard("one.txt");
+            //serializeAllLizards("All.txt", lizards);
+            deserealizeAllLizards("All.txt");
+
+
+        }
+
+        static void serializeOneLizard(string filename, Lizard obj)
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            using (TextWriter textwriter = new StreamWriter(filename))
+            {
+                textwriter.Write(javaScriptSerializer.Serialize(obj));
+            }
+        }
+        static void serializeAllLizards(string filename,List<Lizard> lizards)
+        {
+            JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+            using (TextWriter textWriter=new StreamWriter(filename))
+            {
+                textWriter.Write(javaScriptSerializer.Serialize(lizards));
+            }
+        }
+        static void deserializeOneLizard(string filename)
+        {
+            using (TextReader textreader=new StreamReader(filename))
+            {
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                Lizard lizard = javaScriptSerializer.Deserialize<Lizard>(textreader.ReadToEnd());
+                Console.WriteLine(lizard);
+
+            }
+
+        }
+        static void deserealizeAllLizards(string filename)
+           
+        {
+            using (TextReader textReader=new StreamReader(filename))
+            {
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                List <Lizard> lizards= javaScriptSerializer.Deserialize<List<Lizard>>(textReader.ReadToEnd());
+                foreach(Lizard lizard in lizards)
+                {
+                    Console.WriteLine(lizard);
+                }
+            }
+        }
+    }
+}
